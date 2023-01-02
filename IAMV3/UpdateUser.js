96249ef3a16f68d765b8113d384de6855ab66476
veronika-simic <veronikasimic21@gmail.com>
2023-01-02 11:14:48 +0100

    update user

---
+++ IAMV3/UpdateUser.js
@@ -0,0 +1,20 @@
+import { IAMClient, UpdateUserCommand } from "@aws-sdk/client-iam";
+
+const iamClient = new IAMClient();
+
+const params = {
+  UserName: "test2",
+  NewUserName: "test3",
+};
+
+const run = async () => {
+  try {
+    const data = await iamClient.send(new UpdateUserCommand(params));
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+run();
