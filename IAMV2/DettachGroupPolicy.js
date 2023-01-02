7fd887d84231ace1529fcb93dde10290346e22b9
veronika-simic <veronikasimic21@gmail.com>
2023-01-02 10:08:14 +0100

    detach policy from group

---
+++ IAMV2/DettachGroupPolicy.js
@@ -0,0 +1,15 @@
+const AWS = require("aws-sdk");
+const iam = new AWS.IAM();
+
+const params = {
+  GroupName: "RDSAdmins",
+  PolicyArn: "arn:aws:iam::aws:policy/AmazonRDSFullAccess",
+};
+
+iam.detachGroupPolicy(params, (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
