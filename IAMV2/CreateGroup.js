7cfaeb6143afdc02f3d616320db74f106f2817de
veronika-simic <veronikasimic21@gmail.com>
2023-01-02 09:50:40 +0100

    create groups

---
+++ IAMV2/CreateGroup.js
@@ -0,0 +1,15 @@
+const AWS = require("aws-sdk");
+
+const iam = new AWS.IAM();
+
+const params = {
+  GroupName: "S3Admins",
+};
+
+iam.createGroup(params, (err, data) => {
+  if (err) {
+    console.log(err, err.stack);
+  } else {
+    console.log(data);
+  }
+});
