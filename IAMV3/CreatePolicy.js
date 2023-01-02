cd4c399b469c59abdba10d6196cc2c96edcd0476
veronika-simic <veronikasimic21@gmail.com>
2023-01-02 11:24:09 +0100

    create policy

---
+++ IAMV3/CreatePolicy.js
@@ -0,0 +1,22 @@
+import { IAMClient, CreatePolicyCommand } from "@aws-sdk/client-iam";
+const iamClient = new IAMClient();
+
+const policy_document = {
+  Version: "2012-10-17",
+  Statement: [{ Effect: "Allow", Action: "*", Resource: "*" }],
+};
+
+const params = {
+  PolicyDocument: JSON.stringify(policy_document),
+  PolicyName: "V3FullAccessPolicy",
+};
+const run = async () => {
+  try {
+    const data = await iamClient.send(new CreatePolicyCommand(params));
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+run();
