abb6abd37f401be8cb0c293e4602aa97f0b038e6
veronika-simic <veronikasimic21@gmail.com>
2023-01-02 15:51:43 +0100

    load movies from json file

---
+++ DynamoDB2/LoadMovie.js
@@ -0,0 +1,28 @@
+const AWS = require("aws-sdk");
+AWS.config.update({ region: "us-east-1" });
+const fs = require("fs");
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+console.log("Importing movies data");
+
+const allMovies = JSON.parse(fs.readFileSync("moviedata.json", "utf-8"));
+
+allMovies.forEach(function (movie) {
+  const params = {
+    TableName: "movies",
+    Item: {
+      year: movie.year,
+      title: movie.title,
+      info: movie.info,
+    },
+  };
+
+  docClient.put(params, function (err, data) {
+    if (err) {
+      console.log("Unable to add movie");
+    } else {
+      console.log("Movies added", movie.title);
+    }
+  });
+});
