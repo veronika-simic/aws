27ddcb9ecf96b988dde55491a721c989d06ad416
veronika-simic <veronikasimic21@gmail.com>
2023-01-02 12:05:35 +0100

    delete user

---
+++ IAMV3/DeleteUser.js
@@ -0,0 +1,17 @@
+import { IAMClient, DeleteUserCommand } from "@aws-sdk/client-iam";
+const iamClient = new IAMClient();
+
+const params = {
+  UserName: "test3",
+};
+
+const run = async () => {
+  try {
+    const data = await iamClient.send(new DeleteUserCommand(params));
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+run();
