1ce85c1cb1b172754e2d758c0dceab90fd960f14
veronika-simic <veronikasimic21@gmail.com>
2023-01-02 14:52:57 +0100

    add item with js to dynamo

---
+++ DynamoDB/AddItem.js
@@ -0,0 +1,26 @@
+const AWS = require("aws-sdk");
+AWS.config.update({'region': 'us-east-1'})
+const dynamodb = new AWS.DynamoDB();
+
+const params = {
+  TableName: "employee",
+  Item: {
+    id: {
+      S: "1",
+    },
+    name: {
+      S: "bob",
+    },
+    age: {
+      S: "23",
+    },
+  },
+};
+
+dynamodb.putItem(params, function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
