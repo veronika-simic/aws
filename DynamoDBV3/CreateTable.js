2308540bd417cee32830d5243c50e81649782ab5
veronika-simic <veronikasimic21@gmail.com>
2023-01-03 07:18:24 +0100

    create table with V3

---
+++ DynamoDBV3/CreateTable.js
@@ -0,0 +1,29 @@
+import { DynamoDBClient, CreateTableCommand } from "@aws-sdk/client-dynamodb";
+
+const ddbClient = new DynamoDBClient();
+
+const params = {
+  TableName: "Employee",
+  KeySchema: [
+    {
+      AttributeName: "id",
+      KeyType: "HASH",
+    },
+  ],
+  AttributeDefinitions: [{ AttributeName: "id", AttributeType: "N" }],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 3,
+    WriteCapacityUnits: 3,
+  },
+};
+
+const run = async () => {
+  try {
+    const data = await ddbClient.send(new CreateTableCommand(params));
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+run();
