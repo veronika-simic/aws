694f6e41443a50b34c2003df26b2a8835d995bb8
veronika-simic <veronikasimic21@gmail.com>
2023-01-03 15:03:19 +0100

    create policy

---
+++ S3V3/CreatePolicy.js
@@ -0,0 +1,38 @@
+import { S3Client, PutBucketPolicyCommand } from "@aws-sdk/client-s3";
+
+const s3Client = new S3Client();
+
+const BUCKET_NAME = "verapera24";
+
+const readOnlyUserPolicy = {
+  Version: "2012-10-17",
+  Statement: [
+    {
+      Sid: "AddPerm",
+      Effect: "Allow",
+      Principal: "*",
+      Action: ["s3:GetObject"],
+      Resource: [""],
+    },
+  ],
+};
+
+const bucketResource = "arn:aws:s3:::" + BUCKET_NAME + "/*";
+readOnlyUserPolicy.Statement[0].Resource[0] = bucketResource;
+const bucketPolicyParams = {
+  Bucket: BUCKET_NAME,
+  Policy: JSON.stringify(readOnlyUserPolicy),
+};
+
+const run = async () => {
+  try {
+    const data = await s3Client.send(
+      new PutBucketPolicyCommand(bucketPolicyParams)
+    );
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+run();
