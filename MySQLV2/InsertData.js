0c01b482225346daa80f614d0141176b7a762553
veronika-simic <veronikasimic21@gmail.com>
2023-01-04 08:20:46 +0100

    insert data

--- MySQLV2/InsertData.js
+++ MySQLV2/InsertData.js
@@ -1,5 +1,4 @@
 const mysql = require("mysql");
-
 const connection = mysql.createConnection({
   host: "verapera.cpuwo9cmnsc2.us-east-1.rds.amazonaws.com",
   user: "admin",
@@ -11,9 +10,10 @@ const connection = mysql.createConnection({
 connection.connect(function (err) {
   if (err) throw err;
   console.log("Connected");
-  const sql = "CREATE TABLE customer (name VARCHAR(255), email VARCHAR(255))";
+  const sql =
+    "INSERT INTO customer (name,email) VALUES ('vera', 'eamil@haha.com')";
   connection.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Table created" + result);
+    console.log(result);
   });
 });
