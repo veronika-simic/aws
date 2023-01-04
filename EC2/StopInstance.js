cccfb39109c78a9de11bb6ec149f039bb3488df2
veronika-simic <veronikasimic21@gmail.com>
2023-01-04 10:10:41 +0100

    stop and delete instance

---
+++ EC2/StopInstance.js
@@ -0,0 +1,16 @@
+const AWS = require("aws-sdk");
+AWS.config.update({ region: "us-east-1" });
+
+const ec2 = new AWS.EC2();
+
+const params = {
+  InstanceIds: ["i-0a870c4ca323f9790"],
+};
+
+ec2.stopInstances(params, function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
