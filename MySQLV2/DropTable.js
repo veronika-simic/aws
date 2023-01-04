b4b5db336b72ec52dd7c8551241afdc89ea1483f
veronika-simic <veronikasimic21@gmail.com>
2023-01-04 08:29:33 +0100

    drop table

---
+++ MySQLV2/DropTable.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: "verapera.cpuwo9cmnsc2.us-east-1.rds.amazonaws.com",
+  user: "admin",
+  password: "password",
+  database: "testdb",
+  port: 3306,
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected");
+  const sql = "DROP TABLE customer";
+  connection.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+});
